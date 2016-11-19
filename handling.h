b73043491f50cb1f6642e08358b0d4c9bbc03d3c
ouiqdmw <ouiqdmw17@gmail.com>
2016-11-19 13:47:02 +0900

    Add files via upload
    Usage : ./passer <port #>
    
    For compilation, add -D_REENTRANT macro to gcc.
    ex) gcc passer.c handling.c -D_REENTRANT -o passer -lpthread
    
    By default, there's assumption in the code that the server is in the same ip with the program, and the server port # is 80.
    To reach <serverIP>/<item>, just put <serverIP>:<program port#>/<item> in the browser during the program and server is running.

---
+++ handling.h
@@ -0,0 +1,18 @@
+#ifndef __2016_IS_HANDLING__
+#define __2016_IS_HANDLING__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define MSG_SIZE	(2048)
+#define SERVER_ADDRESS	("127.0.0.1")
+#define SERVER_PORT	(80)
+
+void * handle_clnt(void * arg);
+
+#endif
