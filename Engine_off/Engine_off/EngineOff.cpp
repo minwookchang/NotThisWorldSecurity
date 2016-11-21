57fe2b0428359de856b9492ce4e0b20a492577c0
장민욱 <fromme0528@gmail.com>
2016-11-21 15:15:18 +0900

    str compare&file append

--- Engine_off/Engine_off/EngineOff.cpp
+++ Engine_off/Engine_off/EngineOff.cpp
@@ -1,5 +1,9 @@
-#include "EngineOff.h"
+#define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include "EngineOff.h"
+
 #define CHARPERLINE 500
 #define CHARPERWORD 100
 
@@ -13,10 +17,15 @@ EngineOff::~EngineOff()
 }
 
 
-int EngineOff::exec(const char* _path)
+int EngineOff::exec(char* _path)
 {
+	
+	string str = "<Directory /var/www/html/"+ (string)_path + (string)">";
+	string strend = "</Directory>\0";
+	string stroff = "\tphp_admin_value engine off\0";
+
 	//file read
-	pFile = fopen(_path, "rwb");
+	pFile = fopen(_path, ("rb"));
 
 	long totnum = 0;
 	long curnum = 0;
@@ -24,34 +33,100 @@ int EngineOff::exec(const char* _path)
 
 	_fseeki64(pFile, 0, SEEK_END);
 	long lSize = ftell(pFile);
+	cout << lSize << endl;
 	_fseeki64(pFile, 0, SEEK_SET);
 
-	char* buf = (char*)malloc(sizeof(char)*lSize + 1);
+	char* buf = (char*)malloc(sizeof(char)*lSize);
 	char* buf2 = (char*)malloc(sizeof(char) * CHARPERLINE);
 	char* buf3 = (char*)malloc(sizeof(char) * CHARPERWORD);
+
 	while (curnum = fread(&buf[totnum], sizeof(char), lSize - totnum, pFile) > 0) {
 		totnum += curnum;
 	}
 
-	buf2 = strtok(buf, "\n");
-	
-	while (buf2 = NULL) {
-		if (strcmp(buf2, "<Directory /var/www/html/" + *_path) == 0) {
+	cout << buf << endl;
+
+	getchar();
+
+	buf2 = strtok(buf,"\n");
+
+	cout << "buf2: " << buf2 << endl;
+
+	getchar();
+	//if already exists
+	while (buf2) {
+		
+		cout << buf2 << endl;
+		cout << str << endl;
+		cout << stroff << endl;
+		cout << strend << endl;
+
+		if (strncmp(buf2,str.c_str(),strlen(str.c_str())) == 0) {
 			flag = 1;
+			cout << "flag = 1 " << endl;
 		}
 		if (flag == 1) {
-			if (strcmp(buf2, "php_admin_value engine off") == 0) {
-				return 0;
+			if (strncmp(buf2, stroff.c_str(), strlen(stroff.c_str())) == 0) {
+				flag = 2;
+				cout << "flag = 2 " << endl;
+				break;
+			}
+			else if (strncmp(buf2, strend.c_str(), strlen(strend.c_str())) == 0) {
+				flag = 0;
+				cout << "flag = 0 " << endl;
 			}
 		}
+		getchar();
 		buf2 = strtok(NULL, "\n");
 	}
 
-	//file read
+	//there is already engine off
+	if (flag == 2){
+		return 0;
+
+		free(buf);
+		free(buf2);
+
+	}
+	else if (flag == 0) {
+
+		fclose(pFile);
+
+		pFile = fopen(_path, "a");
+
+		printf("final output\n%s\n%s\n%s", str.c_str(), stroff.c_str(), strend.c_str());
+		fprintf(pFile, "%s\n%s\n%s",str.c_str(),stroff.c_str(),strend.c_str());
+		
+		free(buf);
+		free(buf2);
+		
+		return 1;
+	}
+
+	else {
+		cout << "something wrong" << endl;
+
+		free(buf);
+		free(buf2);
+
+		return 0;
+
+	}
+	//if no engine off
+	//then write
+
+	//이렇게 같은 파일 열고 써도 되나
 
 
-	free(buf);
-	free(buf2);
-	return 0;
+
 }
 
+
+
+/*
+ofstream fout;
+fout.open(_path, ios::app);
+fout << str << "\n\tphp_admin_value engine off\n</Directory>" << endl;
+fout.close();
+
+*/
--- Engine_off/Engine_off/main.cpp
+++ Engine_off/Engine_off/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include "EngineOff.h"
+#define _CRT_SECURE_NO_WARNINGS
+
 using namespace std;
 
-void main(char* argv[]) {
+
+void main() {
 
 
 	 EngineOff engineoff;
 	
-	 int result = engineoff.exec(argv[]);
+	 int result = engineoff.exec("test.txt");
 
 	 if (result == 1) {
 		 cout << "Engine off Success!" << endl;
