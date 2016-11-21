d6ac4adad9ffa2b984c5270c750d521c0d4a0729
knh4735 <skrgus2004@naver.com>
2016-11-21 17:31:55 +0900

    Update safe_upload.php

--- php_library/safe_upload.php
+++ php_library/safe_upload.php
@@ -6,30 +6,30 @@
 
 function safe_upload($file, $path, $max_size = 2147483647){
 	//path validation
-  if(!is_dir($path)) return "존재하지 않는 경로입니다.";
+	if(!is_dir($path)) return "존재하지 않는 경로입니다.";
   
-  //경로 끝에 /가 없으면 추가해줌
-  if($path[strlen($path)-1] !== "/") $path.="/";
+	//경로 끝에 /가 없으면 추가해줌
+	if($path[strlen($path)-1] !== "/") $path.="/";
 
 	if($max_size < $file['size']) return "업로드 파일이 지정된 파일크기보다 큽니다.";
 
 	if(($file['error'] > 0) || ($file['size'] <= 0)) return "파일 업로드에 실패하였습니다.";
 
 	// HTTP post로 전송된 것인지 체크합니다.
-  if(!is_uploaded_file($file['tmp_name'])) return "HTTP로 전송된 파일이 아닙니다.";
+	if(!is_uploaded_file($file['tmp_name'])) return "HTTP로 전송된 파일이 아닙니다.";
 
-  //확장자 체크
+	//확장자 체크
 
 
 	//경로 지정
 	$uploadfile = $path . basename($file['name']);
 
-    // move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다.
-    if(move_uploaded_file($file['tmp_name'], $uploadfile)) {
-        return "성공적으로 업로드 되었습니다.\n";
-    } else {
-        echo "파일 업로드 실패입니다.\n";
-    }
+	// move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다.
+	if(move_uploaded_file($file['tmp_name'], $uploadfile)) {
+	return "성공적으로 업로드 되었습니다.\n";
+	} else {
+	echo "파일 업로드 실패입니다.\n";
+	}
 
 }
 /*
