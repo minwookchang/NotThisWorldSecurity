4cb8bc36a93dd46ec8dcf320e682ddb691f37bb9
sdw0316 <sdw0316@gmail.com>
2016-11-22 22:25:12 +0900

    Update safe_upload.php

--- php_library/safe_upload.php
+++ php_library/safe_upload.php
@@ -12,7 +12,6 @@
 //-4 : 오류
 //-5 : HTTP로 전송된 파일이 아님
 //-6 : php 또는 html 파일
-
 function safe_upload($file, $path, $max_size = 2147483647){
   //path validation
   if(!is_dir($path)) return -2;
@@ -32,24 +31,24 @@ function safe_upload($file, $path, $max_size = 2147483647){
   if(preg_match("/\.(php|html)$/i", $file_name)) return -6;
 
   //경로 지정
-  $uploadfile = $path . $file_name;
+  $path_parts = pathinfo($file_name);
+  $file_name = $path_parts['filename'];
+  $extension = $path_parts['extension'];
+  $uploadfile = $path . sha1($file_name) .".". $extension;
 
   //파일 이름 재설정
-  $file_counter = 1;
-  $path_parts = pathinfo($uploadfile);
-
   while(file_exists($uploadfile)){
-    $uploadfile =  $path . $path_parts['filename'] . " (" . $file_counter++ . ")." . $path_parts['extension'];
+    $file_name = $file_name.rand(1,1000);
+    $uploadfile =  $path . sha1($file_name) . "." . $extension;
   }
 
 
   // move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다.
   if(move_uploaded_file($file['tmp_name'], $uploadfile)) {
-    return realpath($uploadfile);
+    return realpath($path) ."/". $file_name. ".". $extension;
   }
   else {
     return -1;
   }
-
 }
- ?>
+?>
